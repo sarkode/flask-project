b6762efbcce80dfe1101681211d8a138334c2dfc
Sarkodee <adjapongeric1@yahoo.com>
2017-07-14 11:53:30 +0000

    myapp

--- myapp.py
+++ myapp.py
@@ -1,15 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app =Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
-def index ():
-	return "Hello"
-
+def index():
+        return render_template('index.html')
 
 @app.route('/whereami')
 def whereami():
-     return "kdua"
+        return "Kdua"
 
- if __name__ == '__main__':
-         app.run(host="0.0.0.0")
+if __name__ == '__main__':
+        app.run(host="0.0.0.0")
