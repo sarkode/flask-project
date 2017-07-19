d5b5adaf4a36404b4f926ba68f59e932eba4d3bb
Sarkodee <adjapongeric1@yahoo.com>
2017-07-19 16:05:24 +0000

    fixed work

--- myapp.py
+++ myapp.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
+import paho.mqtt.client as mqtt
+
+client= mqtt.Client()
+client.connect("moorhouseassociates.com", 1883, 60)
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return render_template('index.html')
+        return render_template('index.html', to="Sarkodie Eric")
 
 @app.route('/whereami')
 def whereami():
-        return "Kdua"
+    return 'kdua'
 
-if __name__ == '__main__':
-        app.run(host="0.0.0.0")
+
+@app.route('/btn')
+def btn():
+        print ("button clicked")
+	client.publish("test/all","Be vigilant")
+        return ""
+
+@app.route('/linux')
+def linux():
+      return render_template("linux.html")
+
+@app.route("/python")
+def python():
+     return render_template("python.html")
